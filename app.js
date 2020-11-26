599db7de389ae3b4c00823c604e352ec774f8263
adela.dev <adelathomaj@yahoo.com>
2020-11-26 18:05:26 +0100

    changes

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log(AddTwoNumbers(2,2));
+console.log(AddTwoNumbers(3,4));
+PrintHello();
+PrintHello();
+PrintHello();
+let a = 5;
+console.log(a);
+console.log(valueReturned);
+
+let valueReturned = AddTwoNumbers(5,5);
+function AddTwoNumbers(numberOne, numberTwo){
+ return numberOne + numberTwo;
+}
+
+
+function PrintHello(){
+    console.log("Hello!");
+}
